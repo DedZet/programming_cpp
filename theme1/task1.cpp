faef29889295c774f342815b6713579fd5a792d0
DedZet <51733299+DedZet@users.noreply.github.com>
2025-09-25 12:30:30 +1000

    Update task1.cpp

--- theme1/task1.cpp
+++ theme1/task1.cpp
@@ -1,1 +1,32 @@
-<><><>
+#include <iostream>
+
+struct Vector2 {
+	double x;
+	double y;
+};
+
+Vector2 vSum(struct Vector2 a, struct Vector2 b) {
+	struct Vector2 result;
+
+	result.x = a.x + b.x;
+	result.y = a.y + b.y;
+
+	return result;
+}
+
+void printVector(struct Vector2 v) {
+	std::cout << "[" << v.x << " ; " << v.y << "]\n";
+}
+
+int main() {
+
+	struct Vector2 a = {1.24,2};
+	struct Vector2 b = {4, 5.2};
+
+
+
+
+	printVector(vSum(a,b));
+
+	return 0;
+}
