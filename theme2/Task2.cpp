e992c90e31ca48c9e16909b300194fcfeeb70d11
DedZet <51733299+DedZet@users.noreply.github.com>
2025-10-03 18:47:14 +1000

    Create Task2.cpp

---
+++ theme2/Task2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "MyTime.h"
+
+
+int main() {
+    MyTime t1, t2(3,15,30);
+
+    MyTime t3(2, 3, 4);
+
+
+    t3.info();
+    t2.info();
+
+    t1 = t2.mtAdd(t3);
+    t1.info();
+    return 0;
+
+}
