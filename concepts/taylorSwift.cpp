6f3902ccb1b052a3fce33b2ef392f0c778f3a92c
DedZet <51733299+DedZet@users.noreply.github.com>
2025-10-26 18:16:28 +1000

    Update taylorSwift.cpp

--- concepts/taylorSwift.cpp
+++ concepts/taylorSwift.cpp
@@ -1,9 +1,32 @@
+﻿#define _USE_MATH_DEFINES
+
 #include <iostream>
-#include <cmath>
+#include <math.h>
 
-const double PI = 3.1415;
+//typedef double real;
 
-long long factorial(int n) {
+class real
+{
+public:
+    real() { v = 0.; };
+    real(real& that) { this->v = that.v; }
+    real(double that) { v = that; }
+    double ToDouble() { return v; }
+    bool operator ==(real& that) { return v==that.v; }
+    real operator +(real& that) { real res; res.v = v + that.v; return res; }
+    real operator +(double  that) { real res; res.v = v + that; return res;}
+    real operator *(real& that) { real res; res.v = v * that.v; return res;}
+    real operator /(real& that) { real res; res.v = v / that.v; return res;}
+    real& operator +=(real& that) { v = v + that.v; return *this; }
+    real& operator *=(real& that) { v = v * that.v; return *this; }
+    real& operator /=(real& that) { v = v / that.v; return *this; }
+
+public:
+    double v;
+};
+const double PI = M_PI;
+
+long long ___factorial(int n) {
     long long result = 1;
     for (int i = 2; i <= n; i++) {
         result *= i;
@@ -11,33 +34,41 @@ long long factorial(int n) {
     return result;
 }
 
-double TaylorSin(double x, int rows = 10) {
-    
-    x = fmod(x, 2 * PI); // остаток от деления
-    if (x > PI) x -= 2 * PI;
-    if (x < -PI) x += 2 * PI;
+double TaylorSin(real ix) {
+    real x = ix;
+    real result = 0;
+    real term = x;
+    real last = 0;
 
-    double result = 0.0;
-    double firstMem = x;
-    int sign = 1;
+    //double* arr = (double*)malloc(rows * sizeof(double*));
 
-    for (int n = 1; n <= rows * 2; n += 2) {
-        result += sign * firstMem;
-        firstMem *= (x * x) / ((n + 1) * (n + 2));
-        sign *= -1;
+    for (int n = 1; n <= 10000; n += 2) {
+        last = result;
+        result += term;
+
+        if (last == result) {
+            break;
+        }
+
+        term *= (x * x) / real((n + 1) * (n + 2))*(-1);
+        
     }
 
-    return result;
+    return result.ToDouble();
 }
 
 int main() {
     setlocale(LC_ALL, "rus");
 
-    double angle = PI / 4; // 45 градусов
+    double angle = 6; // 45 градусов
 
     std::cout << "Угол: " << angle << " радиан\n";
-    std::cout << "TaylorSin: " << TaylorSin(angle) << std::endl;
-    std::cout << "std::sin: " << std::sin(angle) << std::endl;
+
+    double tsin= TaylorSin(angle);
+    double msin = std::sin(angle);
+
+    printf("\ntsin: %g\nmsin: %g\ndiff: %g\r\n", tsin,msin,tsin-msin);
+
 
     return 0;
 }
