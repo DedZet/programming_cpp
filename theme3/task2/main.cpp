b0bd686492ec661d2973f1712f680dd394de5f09
DedZet <51733299+DedZet@users.noreply.github.com>
2025-10-26 18:19:32 +1000

    Create main.cpp

---
+++ theme3/task2/main.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include "fraction.h"
+
+using namespace std;
+
+int main()
+{
+    Fraction f1(1, 2);
+    Fraction f2 = 3;
+    Fraction f3;
+
+    f3 = f1 + f2;
+    std::cout << f3 << std::endl;
+
+    f3 = f1 + 2;
+    std::cout << f3 << std::endl;
+
+    f3 = 2 + f1;
+    std::cout << f3 << std::endl;
+
+    f3 = f1 - f2;
+    std::cout << f3 << std::endl;
+
+    f3 = f1 - 2;
+    std::cout << f3 << std::endl;
+
+    f3 = 2 - f1;
+    std::cout << f3 << std::endl;
+
+    double x = double(f3);
+    std::cout << x << std::endl;
+ 
+
+    return 0;
+}
