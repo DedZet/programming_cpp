7d30af459d731abca6a8dfb2aac6c9380eb061cd
DedZet <51733299+DedZet@users.noreply.github.com>
2025-10-28 12:15:17 +1000

    Create main.cpp

---
+++ theme3/task2.2/main.cpp
@@ -0,0 +1,24 @@
+﻿#include "Decart.h"
+#include "Polar.h"
+
+#include <iostream>
+
+int main()
+{
+    Decart a(2, 3);
+    Polar b(3, 2);
+
+    std::cout << "Decart point A:" << a << std::endl;
+    std::cout << "Polar point A:" << Polar(a) << "\n\n";
+
+    std::cout << "Polar point B:" << b << std::endl;
+    std::cout << "Decart point B:" << Decart(b) << "\n\n";
+
+    Decart c(b);
+    Polar d(a);
+
+    std::cout << "init C:" << c << std::endl;
+    std::cout << "init D:" << d << std::endl;
+
+    return 0;
+}
